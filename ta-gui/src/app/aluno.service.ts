67c28f743b8bce8cd8d16ef5e4fa243f39ff43ae
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:23:24 -0300

    evitar cadastro de mais de um aluno com o mesmo cpf

--- ta-gui/src/app/aluno.service.ts
+++ ta-gui/src/app/aluno.service.ts
@@ -1,8 +1,15 @@
+import { NonNullAssert } from '@angular/compiler';
 import { Aluno } from './aluno';
 
 export class AlunoService {
   alunos: Aluno[] = [];
-  gravar(aluno: Aluno): void {
-     this.alunos.push(aluno);
+  
+  gravar(aluno: Aluno): Aluno | null {
+    let result : Aluno | null = null;
+    if (aluno != null && !this.alunos.find(a => a.cpf == aluno.cpf)) {
+      this.alunos.push(aluno);
+      result = aluno;
+    }
+    return result;
   }
 }
