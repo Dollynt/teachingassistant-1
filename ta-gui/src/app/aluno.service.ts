ede50e0fa02ee5ecb18c7b76f2a1ce705adfd116
pauloborba <phmb@cin.ufpe.br>
2017-09-05 18:34:00 -0300

    cadastro de metas, e clonagem de objetos para simular funcionamento do servidor

--- ta-gui/src/app/aluno.service.ts
+++ ta-gui/src/app/aluno.service.ts
@@ -5,8 +5,9 @@ import { Aluno } from './aluno';
 @Injectable()
 export class AlunoService {
   alunos: Aluno[] = [];
-  
-  gravar(aluno: Aluno): Aluno | null {
+
+  criar(aluno: Aluno): Aluno | null {
+    aluno = aluno.clone();
     let result : Aluno | null = null;
     if (this.cpfNaoCadastrado(aluno.cpf)) {
       this.alunos.push(aluno);
@@ -19,5 +20,12 @@ export class AlunoService {
      return !this.alunos.find(a => a.cpf == cpf);
   }
 
-
+  atualizar(aluno:Aluno): void {
+    aluno = aluno.clone();
+    for (let a of this.alunos) {
+        if (a.cpf == aluno.cpf) {
+           a.metas = aluno.metas;
+        }
+    }
+  }
 }
--- ta-gui/src/app/aluno.ts
+++ ta-gui/src/app/aluno.ts
@@ -3,4 +3,33 @@ export class Aluno {
   cpf!: string;
   email!: string;
   logingithub!: string;
+  metas!: { [key: string]: string };
+
+  constructor() {
+    this.clean();
+  }
+
+  clean(): void {
+    this.nome = "";
+    this.cpf = "";
+    this.email = "";
+    this.metas = {};
+  }
+
+  clone(): Aluno {
+    var aluno: Aluno = new Aluno();
+    aluno.nome = this.nome;
+    aluno.cpf = this.cpf;
+    aluno.email = this.email;
+    aluno.metas = this.cloneMetas();
+    return aluno;
+  }
+
+  cloneMetas(): { [key: string]: string } {
+    const metas: { [key: string]: string } = {};
+    for (const [key, value] of Object.entries(this.metas)) {
+      metas[key] = value;
+    }
+    return metas;
+  }
 }
